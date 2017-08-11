7e6f5ec5f47b11e7be94e158b55b248a3e43daf0
arunjv <30920256+arunjv@users.noreply.github.com>
2017-08-11 15:29:44 +0530

    Singleton example

---
+++ SingleMain.java
@@ -0,0 +1,38 @@
+package com.arunjv;
+
+public class SingleMain {
+	
+	public static void main(String[] args)
+	{
+		Single s1=Single.getInstance();
+		Single s2=Single.getInstance();
+		Single s3=Single.getInstance();
+		s1.addVisitor();
+		s2.addVisitor();
+		s3.addVisitor();
+	}
+}
+
+
+class Single
+{
+	static Single single;
+	static double visitor;
+	private Single()
+	{
+		System.out.println("Instance created");
+	}
+	public static synchronized Single getInstance()
+	{
+		if(single==null)
+		{
+			single=new Single();
+		}
+		return single;
+	}
+	public void addVisitor()
+	{
+		System.out.println(visitor++);
+	}
+	
+}
